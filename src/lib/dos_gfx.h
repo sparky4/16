fb5c0b9af75a7d052512af5dcd696f17f6a74a48
sparky4 <sparky4@4ch.maidlab.jp>
2012-12-09 13:45:35 -0600

    modified:   .gitignore 	modified:   core16.map 	modified:   src/lib/lib_com.h

--- src/lib/dos_gfx.h
+++ src/lib/dos_gfx.h
@@ -6,15 +6,15 @@
 
 #define SW		320 // width in pixels of vga mode
 #define SH		200 // height in pixels of vga mode
-#define NUM_COLORS	16 // number of colors in vga mode
-static lgq=NUM_COLORS/(1/8)
-static hgq=NUM_COLORS/(1/8)
+#define NUM_COLORS	256 // number of colors in vga mode
+//static lgq=NUM_COLORS/(1/8)
+//static hgq=NUM_COLORS/(1/8)
 /*#define SW		320
 #define SH		200
 #define NUM_COLORS	4*/ // cga
 #define BONK		400
-#define LGQ		lgq
-#define HGQ		hgq
+#define LGQ		32
+#define HGQ		56
 #define VMEM		0xA000 // = vga
 //#define VMEM		0xB800 // = cga
 //#define VMOD		0x13
