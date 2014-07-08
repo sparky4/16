6d2deab47a9764ceb4790e2c97492c74e2d13c5c
sparky4 <sparky4@4ch.maidlab.jp>
2014-07-08 15:40:09 -0500

    new file:   16/modex16/ED.JPG 	new file:   16/modex16/EDw.PCX 	new file:   16/modex16/KOISHI^^.PNG 	modified:   16/modex16/MODEX16.OBJ 	modified:   16/modex16/PCXTEST.C 	modified:   16/modex16/PCXTEST.EXE 	modified:   16/modex16/PCXTEST.OBJ 	modified:   16/modex16/TEST.OBJ 	modified:   16/modex16/koishi^^.pcx

--- 16/modex16/PCXTEST.C
+++ 16/modex16/PCXTEST.C
@@ -5,17 +5,18 @@ void main() {
     bitmap_t bmp;
 	bitmap_t bmp2;
 	bitmap_t bmp3;
+	bitmap_t bmp4;
     int index;
 	int ch=0x0;
 
-    bmp = modexLoadPcx("ed.pcx");
-    bmp2 = modexLoadPcx("ed2.pcx");
-    bmp3 = modexLoadPcx("koishi^^.pcx");
+    //bmp = modexLoadPcx("ed.pcx");
+    //bmp2 = modexLoadPcx("ed2.pcx");
+    bmp4 = modexLoadPcx("koishi^^.pcx");
     modexEnter();
 
     /* fix up the palette and everything */
-    //modexPalUpdate(bmp.palette);
-    modexPalUpdate(bmp2.palette);
+    //modexPalUpdate(bmp2.palette);
+    modexPalUpdate(bmp4.palette);
 
     /* don't show the drawing page */
     modexShowPage(VGA + PAGE_SIZE);
@@ -24,15 +25,19 @@ void main() {
     modexClearRegion(VGA, 0, 0, SCREEN_WIDTH, SCREEN_HEIGHT, 0);
    	modexDrawBmp(VGA, 20, 20, &bmp2, 0);
     modexDrawBmp(VGA, 180, 20, &bmp, 1);
-    modexDrawBmp(VGA, 170, 100, &bmp3, 0);
+    modexDrawBmp(VGA, 0, 0, &bmp3, 1);
+    //modexDrawBmp(VGA, 170, 100, &bmp3, 0);
+	modexDrawBmp(VGA, 100, 100, &bmp4, 1);
+    //modexDrawBmp(VGA, 0, 0, &bmp, 2);
     modexShowPage(VGA);
 //    for(index = 0; index<500; index++) {
-	while(!kbhit()){ // conditions of screen saver
+	while(1/*!kbhit()*/){ // conditions of screen saver
 		modexWaitBorder();
 				ch=getch();
 				if(ch==0x71)break; // 'q'
 				if(ch==0x1b)break; // 'ESC'
 	    }
+	    free();
     modexLeave();
 
     return;
