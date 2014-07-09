4da5d740a876c603807f464dc26d3cff57477441
sparky4 <sparky4@4ch.maidlab.jp>
2014-07-09 14:40:20 -0500

    modified:   16/modex16/MAKEFILE 	modified:   16/modex16/MODEX16.OBJ 	modified:   16/modex16/PCXTEST.C 	modified:   16/modex16/PCXTEST.EXE 	modified:   16/modex16/PCXTEST.OBJ 	modified:   16/modex16/TEST.EXE 	modified:   16/modex16/TEST.OBJ 	new file:   16/modex16/chikyuu.pcx 	new file:   16/modex16/flower.pcx

--- 16/modex16/PCXTEST.C
+++ 16/modex16/PCXTEST.C
@@ -3,41 +3,46 @@
 
 void main() {
     bitmap_t bmp;
-	bitmap_t bmp2;
+	//bitmap_t bmp2;
 	bitmap_t bmp3;
-	bitmap_t bmp4;
-    int index;
-	int ch=0x0;
+	//bitmap_t bmp4;
+//	int index;
+	//int ch=0x0;
 
     //bmp = modexLoadPcx("ed.pcx");
-    //bmp2 = modexLoadPcx("ed2.pcx");
-    bmp4 = modexLoadPcx("koishi^^.pcx");
-    modexEnter();
+//	bmp = modexLoadPcx("koishi^^.pcx");
+	bmp = modexLoadPcx("chikyuu.pcx");
+	bmp3 = modexLoadPcx("flower.pcx");
+    /*bmp2 = modexLoadPcx("ed2.pcx");
+    bmp3 = modexLoadPcx("flower.pcx");
+    bmp4 = modexLoadPcx("koishi^^.pcx");*/
+	modexEnter();
 
     /* fix up the palette and everything */
-    //modexPalUpdate(bmp2.palette);
-    modexPalUpdate(bmp4.palette);
+	modexPalUpdate(bmp3.palette);
 
     /* don't show the drawing page */
     modexShowPage(VGA + PAGE_SIZE);
 
     /* clear and draw one sprite and one bitmap */
     modexClearRegion(VGA, 0, 0, SCREEN_WIDTH, SCREEN_HEIGHT, 0);
-   	modexDrawBmp(VGA, 20, 20, &bmp2, 0);
+   	/*modexDrawBmp(VGA, 20, 20, &bmp2, 0);
     modexDrawBmp(VGA, 180, 20, &bmp, 1);
-    modexDrawBmp(VGA, 0, 0, &bmp3, 1);
-    //modexDrawBmp(VGA, 170, 100, &bmp3, 0);
-	modexDrawBmp(VGA, 100, 100, &bmp4, 1);
-    //modexDrawBmp(VGA, 0, 0, &bmp, 2);
+	modexDrawBmp(VGA, 180, 0, &bmp3, 1);
+	modexDrawBmp(VGA, 170, 100, &bmp4, 1);*/
+	modexDrawBmp(VGA, 0, 0, &bmp, 1);
+	modexDrawBmp(VGA, 100, 20, &bmp3, 1);
     modexShowPage(VGA);
 //    for(index = 0; index<500; index++) {
-	while(1/*!kbhit()*/){ // conditions of screen saver
+	//while(1/*!kbhit()*/){ // conditions of screen saver
+	while (!kbhit()){  /* Wait for a keystroke                         */
 		modexWaitBorder();
-				ch=getch();
-				if(ch==0x71)break; // 'q'
-				if(ch==0x1b)break; // 'ESC'
-	    }
-    modexLeave();
+				//ch=getch();
+				//if(ch==0x71)break; // 'q'
+				//if(ch==0x1b)break; // 'ESC'
+	}
+	(void) getch();   /* Clear the keyboard buffer                    */
+	modexLeave();
 
     return;
 }
