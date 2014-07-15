7e0222fd1ec2920d8c0708357acc6ed194d283f4
sparky4 <sparky4@cock.li>
2014-07-15 12:45:27 -0500

    modified:   .gitignore 	deleted:    16/DOS_GFX.OBJ 	deleted:    16/PCX_LIB/PCX_COMM.OBJ 	deleted:    16/PCX_LIB/PCX_DISP.OBJ 	deleted:    16/PCX_LIB/PCX_EXAM.OBJ 	deleted:    16/PCX_LIB/PCX_FILE.OBJ 	deleted:    16/PCX_LIB/RD_DEMO.OBJ 	deleted:    16/PCX_LIB/WR_DEMO.OBJ 	deleted:    16/SCRC/FUNPTR.OBJ 	deleted:    16/SCRC/PALETTE.OBJ 	deleted:    16/SCRC/TEST.OBJ 	deleted:    16/lib/modex105/DEMOS/BASIC7/MODEX.OBJ 	deleted:    16/lib/modex105/DEMOS/BASIC7/UTILS.OBJ 	deleted:    16/lib/modex105/DEMOS/C/C_UTILS.OBJ 	deleted:    16/lib/modex105/DEMOS/C/MODEX.OBJ 	deleted:    16/lib/modex105/DEMOS/C/X-DEMO.OBJ 	deleted:    16/lib/modex105/DEMOS/QB45/MODEX.OBJ 	deleted:    16/lib/modex105/DEMOS/QB45/UTILS.OBJ 	deleted:    16/lib/modex105/MODEX.OBJ 	deleted:    16/lib/x/MXBB.OBJ 	deleted:    16/lib/x/MXCC.OBJ 	deleted:    16/lib/x/MXCG.OBJ 	deleted:    16/lib/x/MXCL.OBJ 	deleted:    16/lib/x/MXCR.OBJ 	deleted:    16/lib/x/MXFB.OBJ 	deleted:    16/lib/x/MXFP.OBJ 	deleted:    16/lib/x/MXGC.OBJ 	deleted:    16/lib/x/MXGI.OBJ 	deleted:    16/lib/x/MXGM.OBJ 	deleted:    16/lib/x/MXGP.OBJ 	deleted:    16/lib/x/MXGV.OBJ 	deleted:    16/lib/x/MXHL.OBJ 	deleted:    16/lib/x/MXIT.OBJ 	deleted:    16/lib/x/MXLL.OBJ 	deleted:    16/lib/x/MXLN.OBJ 	deleted:    16/lib/x/MXOT.OBJ 	deleted:    16/lib/x/MXPB.OBJ 	deleted:    16/lib/x/MXPF.OBJ 	deleted:    16/lib/x/MXPG.OBJ 	deleted:    16/lib/x/MXPI.OBJ 	deleted:    16/lib/x/MXPN.OBJ 	deleted:    16/lib/x/MXPP.OBJ 	deleted:    16/lib/x/MXRA.OBJ 	deleted:    16/lib/x/MXRP.OBJ 	deleted:    16/lib/x/MXSA.OBJ 	deleted:    16/lib/x/MXSC.OBJ 	deleted:    16/lib/x/MXSI.OBJ 	deleted:    16/lib/x/MXSL.OBJ 	deleted:    16/lib/x/MXSM.OBJ 	deleted:    16/lib/x/MXSP.OBJ 	deleted:    16/lib/x/MXSS.OBJ 	deleted:    16/lib/x/MXTL.OBJ 	deleted:    16/lib/x/MXVS.OBJ 	deleted:    16/lib/x/MXWD.OBJ 	deleted:    16/lib/x/MXWM.OBJ 	deleted:    16/lib/x/MXWP.OBJ 	deleted:    16/lib/x/MXWR.OBJ 	modified:   16/modex16/MAKEFILE 	modified:   16/modex16/MODEX16.OBJ 	modified:   16/modex16/PCXTEST.C 	modified:   16/modex16/PCXTEST.EXE 	modified:   16/modex16/PCXTEST.OBJ 	modified:   16/modex16/TEST.EXE 	modified:   16/modex16/TEST.OBJ 	modified:   16/modex16/chikyuu.pcx 	deleted:    16/scrasm/GENMAP.OBJ 	deleted:    16/scrasm/GENPAL.OBJ 	deleted:    16/scrasm/GENSQ.OBJ 	deleted:    16/scrasm/LZTIMER.OBJ 	deleted:    16/scrasm/MAIN.OBJ 	deleted:    DOS_COMM.OBJ 	deleted:    DOS_OPL2.OBJ 	deleted:    ENG_AI.OBJ 	deleted:    ENG_COMM.OBJ 	deleted:    ENG_CORE.OBJ 	deleted:    ENG_GFX.OBJ 	deleted:    ENG_SND.OBJ 	deleted:    LIB_COM.OBJ 	deleted:    MAIN.OBJ 	deleted:    TIMER.OBJ

--- 16/modex16/PCXTEST.C
+++ 16/modex16/PCXTEST.C
@@ -3,36 +3,46 @@
 
 void main() {
     bitmap_t bmp;
-	bitmap_t bmp2;
-	bitmap_t bmp3;
+	//bitmap_t bmp2;
+	//bitmap_t bmp3;
 	//bitmap_t bmp4;
 //	int index;
 	//int ch=0x0;
 
     //bmp = modexLoadPcx("ed.pcx");
-	bmp = modexLoadPcx("w.pcx");
-	bmp2 = modexLoadPcx("q.pcx");
-	bmp3 = modexLoadPcx("chikyuu-.pcx");
+	//bmp = modexLoadPcx("w.pcx");
+	//bmp2 = modexLoadPcx("q.pcx");
+	printf("=\n");
+	bmp = modexLoadPcx("chikyuu.pcx");
+	printf("==\n");
     /*bmp2 = modexLoadPcx("ed2.pcx");
     bmp3 = modexLoadPcx("flower.pcx");
     bmp4 = modexLoadPcx("koishi^^.pcx");*/
+	printf("===\n");
 	modexEnter();
+	printf("====\n");
 
     /* fix up the palette and everything */
-	//modexPalUpdate(bmp3.palette);
+	printf("=====\n");
+	modexPalUpdate(bmp.palette);
+	printf("======\n");
 
     /* don't show the drawing page */
+//	printf("=======\n");
     modexShowPage(VGA + PAGE_SIZE);
+//	printf("========\n");
 
     /* clear and draw one sprite and one bitmap */
+//	printf("=========\n");
     modexClearRegion(VGA, 0, 0, SCREEN_WIDTH, SCREEN_HEIGHT, 0);
+//	printf("==========\n");
    	/*modexDrawBmp(VGA, 20, 20, &bmp2, 0);
     modexDrawBmp(VGA, 180, 20, &bmp, 1);
 	modexDrawBmp(VGA, 180, 0, &bmp3, 1);
 	modexDrawBmp(VGA, 170, 100, &bmp4, 1);*/
-	modexDrawBmp(VGA, 100, 40, &bmp, 0);
-	modexDrawBmp(VGA, 100, 40, &bmp2, 1);
-	modexDrawBmp(VGA, 0, 20, &bmp3, 0);
+	modexDrawBmp(VGA, 0, 0, &bmp, 1);
+	//modexDrawBmp(VGA, 100, 40, &bmp2, 1);
+	//modexDrawBmp(VGA, 0, 20, &bmp3, 0);
 	//modexDrawBmp(VGA, 120, 60, &bmp3, 1);
     modexShowPage(VGA);
 //    for(index = 0; index<500; index++) {
