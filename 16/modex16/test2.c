c652700ead888913116701f8eaba7893b237fb3a
sparky4 <sparky4@cock.li>
2014-08-27 11:48:04 -0500

    modified:   16/Project 16.bfproject 	modified:   16/modex16/286@12.LOG 	modified:   16/modex16/286@8.LOG 	renamed:    16/modex16_/COMPUTER.PCX -> 16/modex16/COMPUTER.PCX 	renamed:    16/modex16_/EDw.PCX -> 16/modex16/EDw.PCX 	modified:   16/modex16/MAKEFILE 	renamed:    16/modex16_/PALVIEW.EXE -> 16/modex16/PALVIEW.EXE 	modified:   16/modex16/PCXTEST.EXE 	modified:   16/modex16/TEST.C 	modified:   16/modex16/TEST.EXE 	modified:   16/modex16/TEST2.EXE 	renamed:    16/modex16_/chikyuu.pcx -> 16/modex16/chikyuu.pcx 	renamed:    16/modex16_/ed2.pcx -> 16/modex16/ed2.pcx 	renamed:    16/modex16_/koishi.pcx -> 16/modex16/koishi.pcx 	renamed:    16/modex16_/koishi^^.pcx -> 16/modex16/koishi^^.pcx 	renamed:    16/modex16_/koishi~.pcx -> 16/modex16/koishi~.pcx 	modified:   16/modex16/makefile 	modified:   16/modex16/pcxtest.exe 	renamed:    16/modex16_/q.pcx -> 16/modex16/q.pcx 	modified:   16/modex16/test.c 	deleted:    16/modex16/test.exe 	modified:   16/modex16/test2.c 	modified:   16/modex16/test2.exe 	renamed:    16/modex16_/w.pcx -> 16/modex16/w.pcx 	deleted:    16/modex16_/286@12.LOG 	deleted:    16/modex16_/286@8.LOG 	deleted:    16/modex16_/MODEX16.C 	deleted:    16/modex16_/MODEX16.H 	deleted:    16/modex16_/PCXTEST.EXE 	deleted:    16/modex16_/TEST.EXE 	deleted:    16/modex16_/TEST2.EXE 	deleted:    16/modex16_/ed.pcx 	deleted:    16/modex16_/gfx.pal 	deleted:    16/modex16_/makefile 	deleted:    16/modex16_/palettec.c 	deleted:    16/modex16_/pcxtest.c 	deleted:    16/modex16_/test.c 	deleted:    16/modex16_/test.txt 	deleted:    16/modex16_/test2.c 	deleted:    16/modex16_/types.h 	modified:   16proj.bfproject

--- 16/modex16/test2.c
+++ 16/modex16/test2.c
@@ -5,6 +5,7 @@ word far* clock= (word far*) 0x046C; /* 18.2hz clock */
 void main() {
     int i;
     word start;
+    float t1, t2;
     page_t page;
 
     page=modexDefaultPage();
@@ -14,6 +15,8 @@ void main() {
     for(i=0; i<500; i++) {
 	modexShowPage(&page);
     }
+    t1 = (*clock - start)/18.2;
     modexLeave();
 
+    printf("Time: %f\n", t1);
 }
