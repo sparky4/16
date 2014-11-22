092df3c756423869db765af38bd0736a91f12ec1
sparky4 <sparky4@cock.li>
2014-11-22 10:28:48 -0600

    modified:   16/modex16/scroll.c

--- 16/modex16/scroll.c
+++ 16/modex16/scroll.c
@@ -114,7 +114,7 @@ void main() {
 	//when player.tx or player.ty == 0 or player.tx == 20 or player.ty == 15 then stop because that is edge of map and you do not want to walk of the map
 	if(keyp(77)){
 		if(draw->tx >= 0 && draw->tx+20 < MAPX)
-			for(q=0; q<16; q++) {
+			for(q=0; q<TILEWH; q++) {
 				mapScrollRight(draw, SPEED);
 				modexShowPage(draw->page);
 //		mapScrollRight(draw, 1);
@@ -125,7 +125,7 @@ void main() {
 
 	if(keyp(75)){
 		if(draw->tx > 0 && draw->tx+20 <= MAPX)
-			for(q=0; q<16; q++) {
+			for(q=0; q<TILEWH; q++) {
  				mapScrollLeft(draw, SPEED);
 				modexShowPage(draw->page);
 // 		mapScrollLeft(show, 1);
@@ -136,7 +136,7 @@ void main() {
 
 	if(keyp(80)){
 		if(draw->ty >= 0 && draw->ty+15 < MAPY)
-			for(q=0; q<16; q++) {
+			for(q=0; q<TILEWH; q++) {
 				mapScrollDown(draw, SPEED);
 				modexShowPage(draw->page);
 //		mapScrollDown(show, 1);
@@ -147,7 +147,7 @@ void main() {
 
 	if(keyp(72)){
 		if(draw->ty > 0 && draw->ty+15 <= MAPY)
-			for(q=0; q<16; q++) {
+			for(q=0; q<TILEWH; q++) {
 				mapScrollUp(draw, SPEED);
 				modexShowPage(draw->page);
 //		mapScrollUp(show, 1);
