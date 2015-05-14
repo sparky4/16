35bb9977f51839e6b48a65eacbf98aa7a1b19cca
sparky4 <sparky4@cock.li>
2015-05-14 17:10:17 -0500

    hmmm this is an annoying dependancy.... wwww 	new file:   src/lib/exmm/EMM.EXE 	modified:   src/lib/exmm/x.bat

--- src/lib/exmm/x.bat
+++ src/lib/exmm/x.bat
@@ -4,4 +4,4 @@ move emm.exe em0.exe
 move memory.obj memory.ob
 wdis -a memory.ob > memory.asm
 wasm -c -mh -0 memory.asm
-wcp -ecc -mh emm.c memory.obj
+wcp -ecc -mh emm.c memory.obj \z\borlandc\lib\ch.lib
