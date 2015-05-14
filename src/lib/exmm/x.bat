84af3c041adc18905147603fdbd5e4f1b0b7d8cc
sparky4 <sparky4@cock.li>
2015-05-14 13:59:59 -0500

    ems workings~ 	new file:   src/lib/exmm/EM0.EXE 	new file:   src/lib/exmm/MEMORY.ASM 	new file:   src/lib/exmm/MEMORY.MAP 	new file:   src/lib/exmm/MEMORY.OB 	modified:   src/lib/exmm/memory.h 	modified:   src/lib/exmm/x.bat

--- src/lib/exmm/x.bat
+++ src/lib/exmm/x.bat
@@ -1,1 +1,7 @@
-wcp -mh emm.c
+bcc -mh -c -lh memory.c
+bcc -mh emm.c memory.obj
+move emm.exe em0.exe
+move memory.obj memory.ob
+wdis -a memory.ob > memory.asm
+wasm -c -mh -0 memory.asm
+wcp -ecc -mh emm.c memory.obj
