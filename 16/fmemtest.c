6364f4fd0ef7ec33c2feb745d8548f56fb631e3c
sparky4 <sparky4@cock.li>
2015-05-30 07:28:24 -0500

    testing and learning~ 	modified:   16/fmemtest.c 	modified:   16/fmemtest.exe

--- 16/fmemtest.c
+++ 16/fmemtest.c
@@ -5,10 +5,16 @@ main(int argc, char *argv[])
 {
 	int x=420;
 	int huge *ptr=&x;
+	int *ptr0=&x;
 
 	//ptr=&x;
+	printf("ptr0=%Fp\n", ptr0);
+	printf("&ptr0=%Fp\n", &ptr0);
+	printf("*ptr0=%d\n", *ptr0);
 	printf("ptr=%Fp\n", ptr);
+	printf("&ptr=%Fp\n", &ptr);
 	printf("*ptr=%d\n", *ptr);
+	printf("address of *ptr=%Fp\n", *ptr);
 	printf("&x=%Fp\n", &x);
 	printf("x=%d\n", x);
 }
