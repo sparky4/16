bae40ccbdf9b1d39b790d064d69eb4d25b29162d
sparky4 <sparky4@cock.li>
2015-06-05 12:36:13 -0500

    Scroll.exe is seriously broken.... 	modified:   scroll.exe 	modified:   src/scroll.c

--- src/scroll.c
+++ src/scroll.c
@@ -106,9 +106,9 @@ void main() {
 
 	/* create the map */
 //0000	printf("Total used @ before map load:			%zu\n", oldfreemem-GetFreeSize());
-	fprintf(stderr, "testing~\n");
+//0000	fprintf(stderr, "testing~\n");
 //++++	loadmap("data/test.map", &map);
-	fprintf(stderr, "yay map loaded~~\n");
+//0000	fprintf(stderr, "yay map loaded~~\n");
 //----	map = allocMap(map.width,map.height); //20x15 is the resolution of the screen you can make maps smaller than 20x15 but the null space needs to be drawn properly
 	//if(isEMS()) printf("%d tesuto\n", coretotalEMS());
 //----	initMap(&map);
