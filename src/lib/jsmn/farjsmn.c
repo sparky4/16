bb4878674a36456e85b10dc2607444caba597528
sparky4 <sparky4@cock.li>
2015-06-11 14:56:55 -0500

    wwww testing maptest0.exe
    	modified:   data/tes0.map
    	modified:   data/tes0.tmx
    	modified:   emmtest.exe
    	modified:   emsdump.exe
    	modified:   fmemtest.exe
    	modified:   makefile
    	modified:   maptest0.exe
    	modified:   src/lib/jsmn/farjsmn.c

--- src/lib/jsmn/farjsmn.c
+++ src/lib/jsmn/farjsmn.c
@@ -163,8 +163,8 @@ jsmnerr_t jsmn_parse(jsmn_parser huge *parser, const char huge *js, size_t len,
 		char c;
 		jsmntype_t type;
 
-fprintf(stdout, "pee=%lu	count=%u 	i=%d	nt=%u", pee, count, i, num_tokens);
-fprintf(stdout, "	parser->pos=%u\n", parser->pos);
+fprintf(stdout, "%lu	%c count=%u 	i=%d	nt=%u", pee, js[parser->pos], count, i, num_tokens);
+//fprintf(stdout, "token->start=%d\n", token->start);
 pee++;
 
 		c = js[parser->pos];
