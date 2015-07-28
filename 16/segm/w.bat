038ec94a6fc7041ae455704f2334264eef7f7353
sparky4 <sparky4@cock.li>
2015-07-28 11:31:32 -0500

    hmm wcc and wcl do strange things 	modified:   16/segm/WPEE 	modified:   16/segm/w.bat

--- 16/segm/w.bat
+++ 16/segm/w.bat
@@ -3,20 +3,20 @@ echo ==This is the results of open watcom's base pointers== > wpee
 echo These are the various memory models of the 8088 that are demonstrated here! >> wpee
 @del pee.obj
 echo ==tiny is not found so default== >> wpee
-wcc -0 pee.c
+wcl -l=dos -0 pee.c
 pee.exe >> wpee
 echo ==small== >> wpee
-wcc -0 -ms pee.c
+wcl -l=dos -0 -ms pee.c
 pee.exe >> wpee
 echo ==medium== >> wpee
-wcc -0 -mm pee.c
+wcl -l=dos -0 -mm pee.c
 pee.exe >> wpee
 echo ==compact is what project 16 uses== >> wpee
-wcc -0 -mc pee.c
+wcl -l=dos -0 -mc pee.c
 pee.exe >> wpee
 echo ==large== >> wpee
-wcc -0 -ml pee.c
+wcl -l=dos -0 -ml pee.c
 pee.exe >> wpee
 echo ==HUGE GUTS!== >> wpee
-wcl -0 -mh pee.c
+wcl -l=dos -0 -mh pee.c
 pee.exe >> wpee
