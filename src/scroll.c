48577eff32af3269a7c07600575dde555f22f302
sparky4 <sparky4@cock.li>
2015-10-03 12:20:54 -0500

    added fps system ^^

--- src/scroll.c
+++ src/scroll.c
@@ -359,6 +359,18 @@ void main()
 			int 21h
 		}
 	}
+	if(IN_KeyDown(88))	//f12
+	{
+		switch(gvar.fpscap)
+		{
+			case 0:
+				gvar.fpscap=1;
+			break;
+			case 1:
+				gvar.fpscap=0;
+			break;
+		}
+	}
 
 	if((player[0].q==1) && !(player[0].x%TILEWH==0 && player[0].y%TILEWH==0)) break;	//incase things go out of sync!
 	}
