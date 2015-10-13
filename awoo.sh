cc4011ac90b3006bac7f0c4d8c708c8a4be2926d
sparky4 <sparky4@cock.li>
2015-10-13 10:54:52 -0500

    ww

--- awoo.sh
+++ awoo.sh
@@ -2,8 +2,9 @@
 git add .
 . ./boop.sh "$*"
 cp -fp *.exe /var/www/
+scp -rp -P 26 *.exe 4ch.mooo.com:/var/www/16/
 #if [ -f "*.exe" ]
 #	then
-zip /var/www/scroll scroll.exe
-zip /var/www/exmmtest exmmtest.exe
+#zip /var/www/scroll scroll.exe
+#zip /var/www/exmmtest exmmtest.exe
 #fi
