4d25b8cc63c6f23600e6c6f7f1df8d58153bbf9b
sparky4 <sparky4@cock.li>
2015-11-10 16:23:56 -0600

    fack it

--- src/lib/scroll16.c
+++ src/lib/scroll16.c
@@ -488,8 +488,8 @@ mapDrawTile(tiles_t *t, word i, page_t *page, word x, word y)
 				modexClearRegion(page, x, y, t->tileWidth, t->tileHeight, ((t->debug_data[i])+1));
 				//cannot print number value du to it being slow as bakapee
 #else
-				PBUFBFUN(page, x, y, rx, ry, t->tileWidth, t->tileHeight, (t->data));
-				//modexDrawBmpRegion(page, x, y, rx, ry, t->tileWidth, t->tileHeight, (t->btdata));
+				PBUFBFUN		(page, x, y, rx, ry, t->tileWidth, t->tileHeight, (t->data));
+				//modexDrawBmpRegion	(page, x, y, rx, ry, t->tileWidth, t->tileHeight, (t->data));
 #endif
 			break;
 			case 1:
