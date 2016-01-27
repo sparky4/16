2ea85100e642ee29d9cf4df9bc6b7b942cc8f540
sparky4 <sparky4@cock.li>
2016-01-27 10:19:54 -0600

    wwww

--- awoo.sh
+++ awoo.sh
@@ -7,9 +7,9 @@ fi
 ##echo "$pee"
 git add .
 . ./boop.sh "$pee"
-cp -fp *.exe /var/www/
-scp -rp -P 26 *.exe 4ch.mooo.com:/var/www/16/
-#if [ -f "*.exe" ]
+cp -fp bin/*.exe /var/www/
+scp -rp -P 26 bin/*.exe 4ch.mooo.com:/var/www/16/
+#if [ -f "bin/*.exe" ]
 #	then
 #zip /var/www/scroll scroll.exe
 #zip /var/www/exmmtest exmmtest.exe
