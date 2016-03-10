6b54849c9070e295cef52bb6e91d7834a937fa98
sparky4 <sparky4@cock.li>
2016-03-10 16:59:03 -0600

    wwww

--- src/inputest.c
+++ src/inputest.c
@@ -39,7 +39,7 @@ main(int argc, char *argv[])
 		IN_ReadControl(0,&player);
 		//printf("%u\n", IN_KeyDown(sc_Escape));
 		//if(
-		IN_qb(sc_9)//>0) printf("IN_qb(sc_9)=%u\n", IN_qb(sc_9));
+		IN_qb(sc_9);//>0) printf("IN_qb(sc_9)=%u\n", IN_qb(sc_9));
 	}
 	IN_Shutdown();
 	//printf("%u\n", in.Keyboard[sc_Escape]);
