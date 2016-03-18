387784a755423bdeeaa1ad3a4178a009dfbb6687
Jonathan Campbell <jonathan@castus.tv>
2016-03-18 03:44:13 -0700

    cleanup, get working, linking to DOSLIB, also build step to make PCX2VRL sprite compiler, makefile step to produce test VRL+PAL combination for TESUTO.EXE, and DOS batch file to show how to see sample VRL.

--- src/tesuto.h
+++ src/tesuto.h
@@ -7,9 +7,9 @@
 #include "src/lib/doslib/hw/dos/dos.h"
 
 
-typedef unsigned char far *VGA_RAM_PTR;
-VGA_RAM_PTR vga_graphics_ram = (VGA_RAM_PTR)MK_FP(0xA000,0x0000);
-unsigned char vga_stride = 80; // 80 x 4 = 320 for 320-pixel wide modes
+//typedef unsigned char far *VGA_RAM_PTR;
+//VGA_RAM_PTR vga_graphics_ram = (VGA_RAM_PTR)MK_FP(0xA000,0x0000);
+//unsigned char vga_stride = 80; // 80 x 4 = 320 for 320-pixel wide modes
 
 /*static inline void vga_write_sequencer(unsigned char i,unsigned char c) {
     outp(0x3C4,i);
