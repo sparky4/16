2aecf9331d23c0e7d598d024a0d96ffa606b0220
sparky4 <sparky4@cock.li>
2016-04-11 10:20:26 -0500

    wwww

--- spri.bat
+++ spri.bat
@@ -1,2 +1,2 @@
-src\lib\doslib\hw\vga\dos86l\pcxsscut -s data\spri\ptmp.sht -hc data\spri\ptmp.h -hp data\spri\demoanim_ptmp_ -i data\ptmp.pcx -p data\spri\ptmp.pal -tc 0x84 -y # run from subdirectory where output will not be committed accidentally
-src\lib\doslib\hw\vga\dos86l\vrl2vrs -s data\spri\ptmp.sht -hc data\spri\ptmps.h -hp data\spri\demoanim_ptmp_ -o data\spri\ptmp.vrs # run from same subdirectory
+src\lib\doslib\hw\vga\dos86l\pcxsscut -s data\spri\ptmp.sht -hc data\spri\ptmp.h -hp data\spri\demoanim_ptmp_ -i data\ptmp.pcx -p data\spri\ptmp.pal -tc 0x84 -y
+src\lib\doslib\hw\vga\dos86l\vrl2vrs -s data\spri\ptmp.sht -hc data\spri\ptmps.h -hp data\spri\demoanim_ptmp_ -o data\spri\ptmp.vrs
