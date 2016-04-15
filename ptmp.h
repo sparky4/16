5c70a0f11da0d9e7970cc0728c058545092050c8
sparky4 <sparky4@cock.li>
2016-04-15 11:52:25 -0500

    new file:   16.exe 	new file:   bakapi.exe 	deleted:    data/spri/ED.PAL 	deleted:    data/spri/PTMP.PAL 	deleted:    data/spri/PTMP1.PAL 	new file:   data/spri/ptmp.sht 	new file:   data/spri/ptmp.vrs 	new file:   exmmtest.exe 	new file:   fmemtest.exe 	new file:   fontgfx.exe 	new file:   fonttest.exe 	new file:   inputest.exe 	deleted:    makefile 	new file:   maptest.exe 	new file:   palettec.exe 	new file:   palettel.exe 	new file:   pcx2vrl 	new file:   pcxsscut 	new file:   pcxtest.exe 	new file:   pcxtest2.exe 	new file:   planrpcx.exe 	new file:   ptmp.h 	new file:   ptmps.h 	new file:   scroll.exe 	deleted:    spri.bat 	new file:   spri.sh 	modified:   src/inputest.c 	modified:   src/lib/16_in.c 	new file:   test.exe 	new file:   test2.exe 	new file:   tesuto.exe 	new file:   vgmtest.exe 	new file:   vrl2vrs 	new file:   vrsdump

---
+++ ptmp.h
@@ -0,0 +1,8 @@
+// header file for sprite sheet. AUTO GENERATED, do not edit
+// 
+// source PCX: data/ptmp.pcx
+// sheet script: data/spri/ptmp.sht
+// palette file: data/spri/ptmp.pal
+
+
+// end list
---
+++ ptmps.h
@@ -0,0 +1,8 @@
+// header file for sprite sheet. AUTO GENERATED, do not edit
+// 
+// sheet script: data/spri/ptmp.sht
+
+// sprite sheet (sprite IDs)
+// animation list (animation IDs)
+
+// end list
