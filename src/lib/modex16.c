4a133bcbccc48ae5f9e2edb3e9db5fad3e01bb83
sparky4 <sparky4@cock.li>
2016-04-17 21:12:38 -0500

    font fack!

--- src/lib/modex16.c
+++ src/lib/modex16.c
@@ -115,8 +115,7 @@ void modexEnter(sword vq, boolean cmem, global_game_variables_t *gv)
 			cm.clock_select = 0; /* misc register = 0xE3  25MHz */
 			cm.vsync_neg = 1;
 			cm.hsync_neg = 1;
-			vga_state.vga_stride=36;
-			cm.offset=36;
+			vga_state.vga_stride=cm.offset=80;
 			break;
 		case 2: // TODO: 160x120 according to ModeX_160x120regs
 			return;
