2dca5b88958b3206d8706ead3e70b49f9875357f
sparky4 <sparky4@cock.li>
2016-06-16 12:33:59 -0500

    optimize for XT i did ww

---
+++ data/spri/chikyuu.h
@@ -0,0 +1,20 @@
+// header file for sprite sheet. AUTO GENERATED, do not edit
+// 
+// source PCX: ../chikyuu.pcx
+// sheet script: chikyuu.sht
+// palette file: chikyuu.pal
+
+#define _chikyuu_PRSBFCW0 10U
+#define _chikyuu_PRSBFCW1 11U
+#define _chikyuu_PRSBFCW2 12U
+#define _chikyuu_PRSBLCW0 20U
+#define _chikyuu_PRSBLCW1 21U
+#define _chikyuu_PRSBLCW2 22U
+#define _chikyuu_PRSBRCW0 30U
+#define _chikyuu_PRSBRCW1 31U
+#define _chikyuu_PRSBRCW2 32U
+#define _chikyuu_PRSBACW0 40U
+#define _chikyuu_PRSBACW1 41U
+#define _chikyuu_PRSBACW2 42U
+
+// end list
