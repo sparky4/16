0ed2445d78a3165948cd2bddb7cb2da0784a7497
sparky4 <sparky4@cock.li>
2016-06-29 12:10:16 -0500

    wwww

--- data/spri/chikyuu.h
+++ data/spri/chikyuu.h
@@ -4,17 +4,17 @@
 // sheet script: chikyuu.sht
 // palette file: chikyuu.pal
 
-#define _chikyuu_CHIBFCW0 10U
-#define _chikyuu_CHIBFCW1 11U
-#define _chikyuu_CHIBFCW2 12U
-#define _chikyuu_CHIBLCW0 20U
-#define _chikyuu_CHIBLCW1 21U
-#define _chikyuu_CHIBLCW2 22U
-#define _chikyuu_CHIBRCW0 30U
-#define _chikyuu_CHIBRCW1 31U
-#define _chikyuu_CHIBRCW2 32U
-#define _chikyuu_CHIBACW0 40U
-#define _chikyuu_CHIBACW1 41U
-#define _chikyuu_CHIBACW2 42U
+#define _chikyuu_CHUBFCW0 10U
+#define _chikyuu_CHUBFCW1 11U
+#define _chikyuu_CHUBFCW2 12U
+#define _chikyuu_CHUBLCW0 20U
+#define _chikyuu_CHUBLCW1 21U
+#define _chikyuu_CHUBLCW2 22U
+#define _chikyuu_CHUBRCW0 30U
+#define _chikyuu_CHUBRCW1 31U
+#define _chikyuu_CHUBRCW2 32U
+#define _chikyuu_CHUBACW0 40U
+#define _chikyuu_CHUBACW1 41U
+#define _chikyuu_CHUBACW2 42U
 
 // end list
--- data/spri/chikyuus.h
+++ data/spri/chikyuus.h
@@ -3,18 +3,26 @@
 // sheet script: chikyuu.sht
 
 // sprite sheet (sprite IDs)
-#define _chikyuu_CHIBFCW0_sprite 10U
-#define _chikyuu_CHIBFCW1_sprite 11U
-#define _chikyuu_CHIBFCW2_sprite 12U
-#define _chikyuu_CHIBLCW0_sprite 20U
-#define _chikyuu_CHIBLCW1_sprite 21U
-#define _chikyuu_CHIBLCW2_sprite 22U
-#define _chikyuu_CHIBRCW0_sprite 30U
-#define _chikyuu_CHIBRCW1_sprite 31U
-#define _chikyuu_CHIBRCW2_sprite 32U
-#define _chikyuu_CHIBACW0_sprite 40U
-#define _chikyuu_CHIBACW1_sprite 41U
-#define _chikyuu_CHIBACW2_sprite 42U
+#define _chikyuu_CHUBFCW0_sprite 10U
+#define _chikyuu_CHUBFCW1_sprite 11U
+#define _chikyuu_CHUBFCW2_sprite 12U
+#define _chikyuu_CHUBLCW0_sprite 20U
+#define _chikyuu_CHUBLCW1_sprite 21U
+#define _chikyuu_CHUBLCW2_sprite 22U
+#define _chikyuu_CHUBRCW0_sprite 30U
+#define _chikyuu_CHUBRCW1_sprite 31U
+#define _chikyuu_CHUBRCW2_sprite 32U
+#define _chikyuu_CHUBACW0_sprite 40U
+#define _chikyuu_CHUBACW1_sprite 41U
+#define _chikyuu_CHUBACW2_sprite 42U
 // animation list (animation IDs)
+#define _chikyuu_CHUBFCW_STANDING_anim 10U /*frames=1 [ CHUBFCW1@11/event=0/delay=0 ] */
+#define _chikyuu_CHUBFCW_WALKING_anim 11U /*frames=4 [ CHUBFCW0@10/event=0/delay=20 CHUBFCW1@11/event=100/delay=20 CHUBFCW2@12/event=0/delay=20 CHUBFCW1@11/event=100/delay=20 ] */
+#define _chikyuu_CHUBLCW_STANDING_anim 20U /*frames=1 [ CHUBLCW1@21/event=0/delay=0 ] */
+#define _chikyuu_CHUBLCW_WALKING_anim 21U /*frames=4 [ CHUBLCW0@20/event=0/delay=20 CHUBLCW1@21/event=100/delay=20 CHUBLCW2@22/event=0/delay=20 CHUBLCW1@21/event=100/delay=20 ] */
+#define _chikyuu_CHUBRCW_STANDING_anim 30U /*frames=1 [ CHUBRCW1@31/event=0/delay=0 ] */
+#define _chikyuu_CHUBRCW_WALKING_anim 31U /*frames=4 [ CHUBRCW0@30/event=0/delay=20 CHUBRCW1@31/event=100/delay=20 CHUBRCW2@32/event=0/delay=20 CHUBRCW1@31/event=100/delay=20 ] */
+#define _chikyuu_CHUBACW_STANDING_anim 40U /*frames=1 [ CHUBACW1@41/event=0/delay=0 ] */
+#define _chikyuu_CHUBACW_WALKING_anim 41U /*frames=4 [ CHUBACW0@40/event=0/delay=20 CHUBACW1@41/event=100/delay=20 CHUBACW2@42/event=0/delay=20 CHUBACW1@41/event=100/delay=20 ] */
 
 // end list
