291ffe526f0aad4ffe8b6570a9d24636eae40205
sparky4 <sparky4@cock.li>
2016-07-19 08:57:37 -0500

    wwww 16.exe polished wwww and x4get.bat updated to handle more than 10 parts... needs through testingls -lat wwww

--- x4get.bat
+++ x4get.bat
@@ -1,5 +1,7 @@
 @echo off
-del %*
+iff exist %* then
+	del %*
+endiff
 setlocal
 set url="4ch.mooo.com/16"
 rem for %@eval[%#+1] in (%*) do (
@@ -13,17 +15,32 @@ rem for %# in (1,1,%*) do (
 	set /a o=%o+1
 	set y=%w.z%o
 rem	 echo y=%y
-	htget http://%url/%1.zip.00%o > %y
+	if %o lt 100 (
+		if %o lt 10 (
+			set q=00%o
+		) else (
+			set q=0%o
+		) endif
+	) else if %o gt 99 (
+		set q=%o
+	) endif
+
+	htget http://%url/%1.zip.%q > %y
+
 rem  	pause
-	type %y >> %w.zip
+	iff exist %y then
+		type %y >> %w.zip
+	endiff
 	iff "%@FILESIZE[%y,b]" == "65536" then
 rem  		echo o=%o
 		goto oooo
 	endiff
 	iff NOT "%#" == "0" then
 		shift /1
-		unzip %w.zip
-		del %w.z*
+		iff exist %w.zip then
+			unzip %w.zip
+			del %w.z*
+		endiff
 		goto loop
 	endiff
 	endiff
