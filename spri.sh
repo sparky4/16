ac703542eb7799f09575e83f54b60ba9662f5b17
sparky4 <sparky4@cock.li>
2016-09-02 12:14:43 -0500

    wwww made a utility for bcexmm.exe and updated spri.sh

--- spri.sh
+++ spri.sh
@@ -1,6 +1,12 @@
 #! /bin/bash
 cd data/spri
-#../../pcx2vrl -i ../"$1".pcx -o "$1".vrl -p "$1".pal -tc 0x"$2"
-../../pcxsscut -s "$1".sht -hc "$1".h -hp _"$1"_ -i ../"$1".pcx -p "$1".pal -tc 0x"$2" -y
-../../vrl2vrs -s "$1".sht -hc "$1"s.h -hp _"$1"_ -o "$1".vrs
+if [ -f "$1".sht ]; then
+	../../pcxsscut -s "$1".sht -hc "$1".h -hp _"$1"_ -i ../"$1".pcx -p "$1".pal -tc 0x"$2" -y
+	#echo "1st condition ok!"
+else
+	../../pcx2vrl -i ../"$1".pcx -o "$1".vrl -p "$1".pal -tc 0x"$2"
+	#echo "2nd condition ok!"
+	echo check if "$1".sht exist!
+fi
 cd ../../
+
