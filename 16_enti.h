5b5b1d33f20e21199c68d46ae023f26370f1219b
sparky4 <sparky4@cock.li>
2016-11-25 14:59:40 -0600

    wwww 16_entity.h -

---
+++ 16_enti.h
@@ -0,0 +1,396 @@
+rm -f *.err
+wcl -DTARGET_MSDOS=16 -DMSDOS=1 -DDEBUGSERIAL -q -bt=dos -wx -mh -0 -fpi87 -fo=.obj -d1 -wo -i"src/lib/doslib" -obmilr -oe=24 -out -oh -ei -onac -ol+ -ok -sg -st -of+ -zu -zdf -zff -zgf -zq -k32768 -zk0 -zc -zp8 -zm  -c src/16.c
+Error: Compiler returned a bad status compiling 'src/16.c'
+                       Ultimate Packer for eXecutables
+                          Copyright (C) 1996 - 2013
+UPX 3.91        Markus Oberhumer, Laszlo Molnar & John Reiser   Sep 30th 2013
+
+        File size         Ratio      Format      Name
+   --------------------   ------   -----------   -----------
+
+Packed 0 files.
+cat *.err
+src/lib/16_in.h(237): Error! E1022: Missing or misspelled data type near 'entity_t'
+src/16.c(81): Error! E1151: Parameter count does not agree with previous definition
+src/16.c(81): Note! I2002: 'IN_ReadControl' defined in: src/lib/16_in.h(291)
+
+7-Zip 9.20  Copyright (c) 1999-2010 Igor Pavlov  2010-11-18
+p7zip Version 9.20 (locale=en_US.UTF-8,Utf16=on,HugeFiles=on,2 CPUs)
+Scanning
+
+
+16.exe:  WARNING: No more files                
+
+
+Creating archive /var/www/16.exe.zip
+
+
+
+WARNINGS for files:
+
+16.exe : No more files                
+----------------
+WARNING: Cannot find 1 file
+7z a -v64k /var/www/16.exe.zip 16.exe
+
+7-Zip 9.20  Copyright (c) 1999-2010 Igor Pavlov  2010-11-18
+p7zip Version 9.20 (locale=en_US.UTF-8,Utf16=on,HugeFiles=on,2 CPUs)
+Scanning
+
+
+bakapi.exe:  WARNING: No more files                
+
+
+Creating archive /var/www/bakapi.exe.zip
+
+
+
+WARNINGS for files:
+
+bakapi.exe : No more files                
+----------------
+WARNING: Cannot find 1 file
+7z a -v64k /var/www/bakapi.exe.zip bakapi.exe
+
+7-Zip 9.20  Copyright (c) 1999-2010 Igor Pavlov  2010-11-18
+p7zip Version 9.20 (locale=en_US.UTF-8,Utf16=on,HugeFiles=on,2 CPUs)
+Scanning
+
+
+palettel.exe:  WARNING: No more files                
+
+
+Creating archive /var/www/palettel.exe.zip
+
+
+
+WARNINGS for files:
+
+palettel.exe : No more files                
+----------------
+WARNING: Cannot find 1 file
+7z a -v64k /var/www/palettel.exe.zip palettel.exe
+
+7-Zip 9.20  Copyright (c) 1999-2010 Igor Pavlov  2010-11-18
+p7zip Version 9.20 (locale=en_US.UTF-8,Utf16=on,HugeFiles=on,2 CPUs)
+Scanning
+
+
+palettec.exe:  WARNING: No more files                
+
+
+Creating archive /var/www/palettec.exe.zip
+
+
+
+WARNINGS for files:
+
+palettec.exe : No more files                
+----------------
+WARNING: Cannot find 1 file
+7z a -v64k /var/www/palettec.exe.zip palettec.exe
+
+7-Zip 9.20  Copyright (c) 1999-2010 Igor Pavlov  2010-11-18
+p7zip Version 9.20 (locale=en_US.UTF-8,Utf16=on,HugeFiles=on,2 CPUs)
+Scanning
+
+
+tesuto.exe:  WARNING: No more files                
+
+
+Creating archive /var/www/tesuto.exe.zip
+
+
+
+WARNINGS for files:
+
+tesuto.exe : No more files                
+----------------
+WARNING: Cannot find 1 file
+7z a -v64k /var/www/tesuto.exe.zip tesuto.exe
+
+7-Zip 9.20  Copyright (c) 1999-2010 Igor Pavlov  2010-11-18
+p7zip Version 9.20 (locale=en_US.UTF-8,Utf16=on,HugeFiles=on,2 CPUs)
+Scanning
+
+
+0.exe:  WARNING: No more files                
+
+
+Creating archive /var/www/0.exe.zip
+
+
+
+WARNINGS for files:
+
+0.exe : No more files                
+----------------
+WARNING: Cannot find 1 file
+7z a -v64k /var/www/0.exe.zip 0.exe
+
+7-Zip 9.20  Copyright (c) 1999-2010 Igor Pavlov  2010-11-18
+p7zip Version 9.20 (locale=en_US.UTF-8,Utf16=on,HugeFiles=on,2 CPUs)
+Scanning
+
+
+scroll.exe:  WARNING: No more files                
+
+
+Creating archive /var/www/scroll.exe.zip
+
+
+
+WARNINGS for files:
+
+scroll.exe : No more files                
+----------------
+WARNING: Cannot find 1 file
+7z a -v64k /var/www/scroll.exe.zip scroll.exe
+
+7-Zip 9.20  Copyright (c) 1999-2010 Igor Pavlov  2010-11-18
+p7zip Version 9.20 (locale=en_US.UTF-8,Utf16=on,HugeFiles=on,2 CPUs)
+Scanning
+
+
+zcroll.exe:  WARNING: No more files                
+
+
+Creating archive /var/www/zcroll.exe.zip
+
+
+
+WARNINGS for files:
+
+zcroll.exe : No more files                
+----------------
+WARNING: Cannot find 1 file
+7z a -v64k /var/www/zcroll.exe.zip zcroll.exe
+
+7-Zip 9.20  Copyright (c) 1999-2010 Igor Pavlov  2010-11-18
+p7zip Version 9.20 (locale=en_US.UTF-8,Utf16=on,HugeFiles=on,2 CPUs)
+Scanning
+
+
+exmmtest.exe:  WARNING: No more files                
+
+
+Creating archive /var/www/exmmtest.exe.zip
+
+
+
+WARNINGS for files:
+
+exmmtest.exe : No more files                
+----------------
+WARNING: Cannot find 1 file
+7z a -v64k /var/www/exmmtest.exe.zip exmmtest.exe
+
+7-Zip 9.20  Copyright (c) 1999-2010 Igor Pavlov  2010-11-18
+p7zip Version 9.20 (locale=en_US.UTF-8,Utf16=on,HugeFiles=on,2 CPUs)
+Scanning
+
+
+vrstest.exe:  WARNING: No more files                
+
+
+Creating archive /var/www/vrstest.exe.zip
+
+
+
+WARNINGS for files:
+
+vrstest.exe : No more files                
+----------------
+WARNING: Cannot find 1 file
+7z a -v64k /var/www/vrstest.exe.zip vrstest.exe
+
+7-Zip 9.20  Copyright (c) 1999-2010 Igor Pavlov  2010-11-18
+p7zip Version 9.20 (locale=en_US.UTF-8,Utf16=on,HugeFiles=on,2 CPUs)
+Scanning
+
+
+16.exe:  WARNING: No more files                
+
+
+Creating archive /var/www/16.exe.zip
+
+
+
+WARNINGS for files:
+
+16.exe : No more files                
+----------------
+WARNING: Cannot find 1 file
+7z a -v64k /var/www/16.exe.zip 16.exe
+
+7-Zip 9.20  Copyright (c) 1999-2010 Igor Pavlov  2010-11-18
+p7zip Version 9.20 (locale=en_US.UTF-8,Utf16=on,HugeFiles=on,2 CPUs)
+Scanning
+
+
+bakapi.exe:  WARNING: No more files                
+
+
+Creating archive /var/www/bakapi.exe.zip
+
+
+
+WARNINGS for files:
+
+bakapi.exe : No more files                
+----------------
+WARNING: Cannot find 1 file
+7z a -v64k /var/www/bakapi.exe.zip bakapi.exe
+
+7-Zip 9.20  Copyright (c) 1999-2010 Igor Pavlov  2010-11-18
+p7zip Version 9.20 (locale=en_US.UTF-8,Utf16=on,HugeFiles=on,2 CPUs)
+Scanning
+
+
+palettel.exe:  WARNING: No more files                
+
+
+Creating archive /var/www/palettel.exe.zip
+
+
+
+WARNINGS for files:
+
+palettel.exe : No more files                
+----------------
+WARNING: Cannot find 1 file
+7z a -v64k /var/www/palettel.exe.zip palettel.exe
+
+7-Zip 9.20  Copyright (c) 1999-2010 Igor Pavlov  2010-11-18
+p7zip Version 9.20 (locale=en_US.UTF-8,Utf16=on,HugeFiles=on,2 CPUs)
+Scanning
+
+
+palettec.exe:  WARNING: No more files                
+
+
+Creating archive /var/www/palettec.exe.zip
+
+
+
+WARNINGS for files:
+
+palettec.exe : No more files                
+----------------
+WARNING: Cannot find 1 file
+7z a -v64k /var/www/palettec.exe.zip palettec.exe
+
+7-Zip 9.20  Copyright (c) 1999-2010 Igor Pavlov  2010-11-18
+p7zip Version 9.20 (locale=en_US.UTF-8,Utf16=on,HugeFiles=on,2 CPUs)
+Scanning
+
+
+tesuto.exe:  WARNING: No more files                
+
+
+Creating archive /var/www/tesuto.exe.zip
+
+
+
+WARNINGS for files:
+
+tesuto.exe : No more files                
+----------------
+WARNING: Cannot find 1 file
+7z a -v64k /var/www/tesuto.exe.zip tesuto.exe
+
+7-Zip 9.20  Copyright (c) 1999-2010 Igor Pavlov  2010-11-18
+p7zip Version 9.20 (locale=en_US.UTF-8,Utf16=on,HugeFiles=on,2 CPUs)
+Scanning
+
+
+0.exe:  WARNING: No more files                
+
+
+Creating archive /var/www/0.exe.zip
+
+
+
+WARNINGS for files:
+
+0.exe : No more files                
+----------------
+WARNING: Cannot find 1 file
+7z a -v64k /var/www/0.exe.zip 0.exe
+
+7-Zip 9.20  Copyright (c) 1999-2010 Igor Pavlov  2010-11-18
+p7zip Version 9.20 (locale=en_US.UTF-8,Utf16=on,HugeFiles=on,2 CPUs)
+Scanning
+
+
+scroll.exe:  WARNING: No more files                
+
+
+Creating archive /var/www/scroll.exe.zip
+
+
+
+WARNINGS for files:
+
+scroll.exe : No more files                
+----------------
+WARNING: Cannot find 1 file
+7z a -v64k /var/www/scroll.exe.zip scroll.exe
+
+7-Zip 9.20  Copyright (c) 1999-2010 Igor Pavlov  2010-11-18
+p7zip Version 9.20 (locale=en_US.UTF-8,Utf16=on,HugeFiles=on,2 CPUs)
+Scanning
+
+
+zcroll.exe:  WARNING: No more files                
+
+
+Creating archive /var/www/zcroll.exe.zip
+
+
+
+WARNINGS for files:
+
+zcroll.exe : No more files                
+----------------
+WARNING: Cannot find 1 file
+7z a -v64k /var/www/zcroll.exe.zip zcroll.exe
+
+7-Zip 9.20  Copyright (c) 1999-2010 Igor Pavlov  2010-11-18
+p7zip Version 9.20 (locale=en_US.UTF-8,Utf16=on,HugeFiles=on,2 CPUs)
+Scanning
+
+
+exmmtest.exe:  WARNING: No more files                
+
+
+Creating archive /var/www/exmmtest.exe.zip
+
+
+
+WARNINGS for files:
+
+exmmtest.exe : No more files                
+----------------
+WARNING: Cannot find 1 file
+7z a -v64k /var/www/exmmtest.exe.zip exmmtest.exe
+
+7-Zip 9.20  Copyright (c) 1999-2010 Igor Pavlov  2010-11-18
+p7zip Version 9.20 (locale=en_US.UTF-8,Utf16=on,HugeFiles=on,2 CPUs)
+Scanning
+
+
+vrstest.exe:  WARNING: No more files                
+
+
+Creating archive /var/www/vrstest.exe.zip
+
+
+
+WARNINGS for files:
+
+vrstest.exe : No more files                
+----------------
+WARNING: Cannot find 1 file
+7z a -v64k /var/www/vrstest.exe.zip vrstest.exe
+rm -f *.err
