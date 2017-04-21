6c98a0c193de37421dabbe746a0b699020b5c8af
sparky4 <sparky4@cock.li>
2017-04-21 08:18:27 -0500

    p16 is being worked on a bunch by me wwww [16_ca needs huge amounts of work and I should remember what needs to be done soon][OpenVGMFile needs to be ported to 16_snd.c]going to port rest of code to borland c some time so we can use the core components of id engine here [going to add 16_us.c eventually but the debug system and CA_ PM_ and MM_ usage is priority now]older zcroll renamed to xcroll][zcroll is now the pre menu game loop system with PROPER data usage with CAMMPM] MM_ShowMemory works how i want wwww [0croll.exe works better now]

---
+++ doc/newpack.sh
@@ -0,0 +1,3 @@
+apt-get install toilet tint schism cowsay sl fortune fonts-mona xfonts-mona beep milkytracker screenfetch weechat figlet linuxlogo xcowsay sudo dosbox htop gparted gpart testdisk upx itop cmatrix oneko bsdgames games-toys ncdu cmatrix-xfont doschk dosfstools curlftpfs hasciicam xclip screen openssh-server tiled
+apt-get install unattended-upgrades
+dpkg-reconfigure unattended-upgrades
