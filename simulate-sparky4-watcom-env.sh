68ed07e129d9504e398bc771082352ee0f591d85
Jonathan Campbell <jonathan@castus.tv>
2017-09-08 13:05:06 -0700

    okay sparky let's get down to what's making your build environment screw up with doslib

---
+++ simulate-sparky4-watcom-env.sh
@@ -0,0 +1,58 @@
+# Source: http://4ch.mooo.com/4/watcom/wat.sh
+# with modifications to simulate this environment on Jon's Linux system
+#
+# Note that nothing in this project appears to use ROOT or REXX variables
+# ------BEGIN-------
+#echo Open Watcom Build Environment
+ROOT=/home/sparky4/p/osfree
+watcomver=""
+watcom=/dos/fdos/watcom${watcomver}
+WATCOM=$watcom
+OS=LINUX
+ENV=LINUX
+SHELL=Bourne
+REXX=/usr/bin/rexx
+
+
+if [ "${watcomver}" == 2 ]; then
+	MACHINE_TYPE=`uname -m`
+	if [ ${MACHINE_TYPE} == 'x86_64' ]; then
+	        PEE="64"
+	else
+	        PEE=""
+	fi
+else
+	PEE=""
+fi
+
+TOOLS=$ROOT/bin/tools
+INCLUDE=$watcom/h:$watcom/h/dos:$watcom/lh:$watcom/h/os2
+WIPFC=$watcom/wipfc
+LIB=$watcom/lib286/dos:$watcom/lib386:$watcom/lib386/dos:$watcom/lib286:$ROOT/lib
+export OS REXX WATCOM ROOT TOOLS INCLUDE LIB WIPFC
+. ~/setvars.sh
+PATH=$watcom/binl${PEE}:$TOOLS:$ROOT:$PATH
+WD_PATH=$watcom/binl${PEE}
+
+TERM=xterm-16color
+export OS ENV REXX WATCOM ROOT TOOLS INCLUDE LIB PATH WD_PATH WIPFC TERM
+
+alias wcp="wcl -lr -l=dos -0 "
+if [ ! -d "$watcom" ]; then
+	echo "INSTALL OPEN WATCOM AND EDIT THIS FILE TO THE CORRESPONDING DIRECTORY!!!!"
+fi
+
+#if [ ! -d "$watcom/binl64" ]; then
+#	ln -s $watcom/binl64 $watcom/binl
+#fi
+
+#if [  -e "$HOME/setvars.sh" ]; then
+#	length=$ROOT
+#	((length--))
+#	[ "${filename:$length:1}" == "2" ] && . ./setvars.sh
+#	else if [ ! -e "setvars.sh" ]; then
+#	wget http://4ch.mooo.com/4/watcom/setvars.sh
+#	chmod +x ~/setvars.sh
+#fi
+#fi
+#. ~/aliasyonet.sh
