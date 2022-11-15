d4ebd940c098956c346850db0b0c72ee790bcbf6
sparky4 <sparky4@cock.li>
2022-11-14 20:05:04 -0600

    undid undid xD

--- 16/sod8086/wl_act2.c
+++ 16/sod8086/wl_act2.c
@@ -1936,7 +1936,7 @@ void A_Dormant (objtype *ob)
 	long	deltax,deltay;
 	int	xl,xh,yl,yh;
 	int	x,y;
-	unsigned	tile;
+	objtype	*tile;
 
 	deltax = ob->x - player->x;
 	if (deltax < -MINACTORDIST || deltax > MINACTORDIST)
@@ -1959,7 +1959,7 @@ moveok:
 			tile = actorat[x][y];
 			if (!tile)
 				continue;
-			if (tile<256)
+			if (tile && tile<objlist)
 				return;
 			if (((objtype *)tile)->flags&FL_SHOOTABLE)
 				return;
